29a7cced052416ed4eafb967e969ca8ef92795dd
rahnamafard <arahnamafard@yahoo.com>
2020-02-06 18:51:34 +0330

    Filter by geo-location

--- client/src/ControlForm.js
+++ client/src/ControlForm.js
@@ -51,12 +51,16 @@ class ControlForm extends Component {
         this.state = {
             title: "Sample form",
             form_id: null,
+            form_fields: null,
             responses: null,
-            ready: false,
             filter: {
                 name: null, // Field name
-                type: null // Field type
-            }
+                type: null, // Field type
+                value: null, // Polygon
+                gt: null,
+                lt: null,
+            },
+            ready: false,
         }
     }
 
@@ -85,6 +89,41 @@ class ControlForm extends Component {
         this.setState({
             title: "First Form",
             form_id: fid,
+            form_fields:
+                [
+                    {
+                        name: "Birth_Date",
+                        title: "Birth Date",
+                        type: "Date",
+                        required: true,
+                    },
+                    {
+                        name: "Number",
+                        title: "Number",
+                        type: "Number",
+                        required: true,
+                    },
+                    {
+                        name: "Request_Type",
+                        title: "Request Type",
+                        type: "Text",
+                        options:
+                            [
+                                { label: "Help", value: "Help" },
+                                { label: "Info", value: "Information" }
+                            ],
+                    },
+                    {
+                        name: "Home",
+                        title: "خانه",
+                        type: "Location",
+                    },
+                    {
+                        name: "Work",
+                        title: "محل کار",
+                        type: "Location",
+                    },
+                ],
             responses:
                 [
                     {
@@ -221,15 +260,86 @@ class ControlForm extends Component {
     }
 
     handleFilterBySelect = event => {
-        const option = event.target;
-
-        this.state
+        let field = this.state.form_fields.find(field => field.name === event.target.value);
 
         this.setState({
             filter: {
-                name: option.name,
-                type: option.type
+                name: field.name,
+                type: field.type,
+                value: null,
             }
+        })
+    }
+
+    handleLocationSelect = event => {
+        this.setState({
+            filter: {
+                value: event.target.value
+            }
+        });
+    }
+
+    applyFilter = event => {
+        // TODO: API Call to apply filter with query
+
+        // response 1 deleted for example
+        this.setState({
+            responses:
+            [
+                {
+                    response_id: 2,
+                    fields:
+                        [
+                            {
+                                name: "Birth_Date",
+                                title: "Birth Date",
+                                type: "Date",
+                                required: true,
+                                value: "2020-02-18T20:30:00.000Z"
+
+                            },
+                            {
+                                name: "Number",
+                                title: "Number",
+                                type: "Number",
+                                required: true,
+                                value: "5"
+
+                            },
+                            {
+                                name: "Request_Type",
+                                title: "Request Type",
+                                type: "Text",
+                                options:
+                                    [
+                                        { label: "Help", value: "Help" },
+                                        { label: "Info", value: "Information" }
+                                    ],
+                                value: "\"Help\""
+                            },
+                            {
+                                name: "Home",
+                                title: "خانه",
+                                type: "Location",
+                                label: "خیابان 1",
+                                value: {
+                                    "lat": 35.618974646696394,
+                                    "long": 51.36702734375001
+                                },
+                            },
+                            {
+                                name: "Work",
+                                title: "محل کار",
+                                type: "Location",
+                                label: "خیابان 2",
+                                value: {
+                                    "lat": 55.618974646696394,
+                                    "long": 61.36702734375001
+                                },
+                            },
+                        ],
+                },
+            ]
         })
     }
 
@@ -295,19 +405,22 @@ class ControlForm extends Component {
             if(this.state.filter.type === "Location")
             {
                 filter_details =
-                    <FormControl>
-                        <InputLabel ref="" id="demo-simple-select-outlined-label">
-                            در منطقه:
-                        </InputLabel>
-                        <Select
-                            name="area"
-                            autoWidth={true}
-                        >
-                            {filterby.map(item => (
-                                <MenuItem value={item.name}>{item.title}</MenuItem>
-                            ))}                 
-                        </Select>
-                    </FormControl>
+                    <div>
+                        <FormControl>
+                            <InputLabel ref="" id="demo-simple-select-outlined-label">
+                                در منطقه:
+                            </InputLabel>
+                            <Select
+                                name="area"
+                                autoWidth={true}
+                            >
+                                {this.state.polygons.map(poly => (
+                                    <MenuItem selected={this.state.filter.area === poly.polygon_id} value={poly.polygon_id}>{poly.name}</MenuItem>
+                                ))}                 
+                            </Select>
+                        </FormControl>
+                        <br /><br />
+                    </div>
             }
 
 
@@ -374,8 +487,15 @@ class ControlForm extends Component {
                                 </FormControl>
 
                                 <br />
+                                <br />
 
                                 {filter_details}
+
+                                <FormControl>
+                                    <Button variant="contained" color="primary" onClick={(e) => this.applyFilter(e)}>
+                                        فیلتر کن
+                                    </Button>
+                                </FormControl>
 
                             </MuiThemeProvider>
                         </form>
