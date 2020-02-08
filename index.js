ae4e33f5a67432a2f0d752bcb12122c67d1f451c
rahnamafard <arahnamafard@yahoo.com>
2020-02-09 00:23:31 +0330

    useNewUrlParser true

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ const app = express();
 
 mongoose.Promise = global.Promise;
 //mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/final`);
-mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://mmd:ssd@cluster0-sfn9i.mongodb.net/test?retryWrites=true&w=majority`);
+mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://mmd:ssd@cluster0-sfn9i.mongodb.net/test?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(bodyParser.json());
 app.use(cors());
