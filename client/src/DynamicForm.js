9c64dc3e3e58ac3e297eb6b209f59127aaae16aa
noyz <mahdian.navid@gmail.com>
2020-02-08 19:08:03 +0330

    Location label pushed in value the way backend wants

--- client/src/DynamicForm.js
+++ client/src/DynamicForm.js
@@ -90,9 +90,11 @@ export default class DynamicForm extends React.Component {
       }
     }
     else if(type === "LocationOption"){
+      let valueWithoutLabel = JSON.parse(JSON.stringify(e.target.value));
+      valueWithoutLabel.label = text;
       this.setState(
         {
-          [key]:{ label : text  ,value: Object.assign(JSON.parse(e.target.value), {label: text}) }
+          [key]:{ label : text  ,value : valueWithoutLabel }
         },
         () => {}
       );
